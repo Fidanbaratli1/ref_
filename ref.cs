dcde4b7fb6340e1fe8c2812dbe48a574babd3915
Fidanbaratli1 <fidanbaratli@gmail.com>
2023-03-29 22:17:28 +0400

    created file

---
+++ ref.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 5, 6, 7, 8 };
+            InsertArray(ref  numbers,12,13);
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+           
+        }
+        public static void InsertArray(ref int[] arr, params int[] nums) 
+        {
+            
+            int[] numbers2= new int[arr.Length+nums.Length];
+            for (int i=0 ; i < arr.Length; i++)
+            {
+                numbers2[i] = arr[i];
+                numbers2[i+nums.Length]=i;
+               
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numbers2[arr.Length + i] = nums[i];
+            }
+
+            arr = numbers2;
+        }
+    }
+}
